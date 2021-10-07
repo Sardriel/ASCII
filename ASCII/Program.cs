214f8b862add69ee5da2f57e972b815022e463ea
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-10-07 10:55:38 +0200

    Add project files.

---
+++ ASCII/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c; // znak
+            int i; // ordinální (ASCII) hodnota znaku
+            //převedeme znak na jeho ASCII hodnotu
+            c = 'a';
+            i = (int)c;
+            Console.WriteLine("Znak {0} jsme převedli na ASCII hodnotu {1}", c, i);
+            //Převedem ASCII hodnotu na znak
+            i = 98;
+            c = (char)i;
+            Console.WriteLine("ASCII hodnotu {1} jsme převedli na znak {0}", c, i);
+            Console.ReadKey();
+        }
+    }
+}
